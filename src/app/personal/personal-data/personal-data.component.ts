1e2b7f002075e4a61d614af3bfde657657857a3e
Adrian Zawadzki <zawadzkii@tlen.pl>
2024-02-11 16:40:02 +0100

    personal

--- src/app/personal/personal-data/personal-data.component.ts
+++ src/app/personal/personal-data/personal-data.component.ts
@@ -10,18 +10,10 @@ export class PersonalDataComponent  implements OnInit {
   
   currentUser: User = new User;
 
-  constructor() {
-   
-  }
-
   ngOnInit() {
-    console.log("ABC")
     const storageUserAsStr = localStorage.getItem('currentUser');
-    console.log("ABC2", storageUserAsStr)
 
     if(storageUserAsStr) {
-      console.log("ABC", storageUserAsStr)
-
       let storageUser = JSON.parse(storageUserAsStr);
       this.currentUser = storageUser;
     }
